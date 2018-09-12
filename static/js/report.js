47875baf82c8cc409b4a83783fdb2e55cdc8e494
lorenz <lorenzlabby@gmail.com>
2018-09-12 22:00:16 +0300

    commit

--- static/js/report.js
+++ static/js/report.js
@@ -1,7 +1,7 @@
 var conn = new ab.Session('ws://149.28.138.51/ws2/:5555',
     function(){
         conn.subscribe('report', function(topic, data){
-            console.log('New article published to category ' + topic + ' : ' + data.data);
+            console.log('New article published to category ' + topic + ' : ' + data.data.dbuuid + "  " + data.data['dbuuid']);
         });
     },
 
