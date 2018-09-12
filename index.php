64b51dada0f51491d0df9e8757ad33468ac735d1
lorenz <lorenzlabby@gmail.com>
2018-09-12 12:01:42 +0300

    first commit

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-        <h1>Hello</h1>
-<?php require(dirname(__DIR__) . "/inc/header.php"); ?>
+<?php echo "he"; ?>
+<!-- <?php require(dirname(__DIR__) . "/inc/header.php"); ?>
 
         <h1>Hello</h1>
 
-<?php require(dirname(__DIR__) . "/inc/footer.php"); ?>
+<?php require(dirname(__DIR__) . "/inc/footer.php"); ?> -->
