696f9763ce5d4e8e20649846903c6c26beb83279
lorenz <lorenzlabby@gmail.com>
2018-09-12 13:18:29 +0300

    realtime

--- bin/server.php
+++ bin/server.php
@@ -8,7 +8,7 @@
 
     $context = new React\ZMQ\Context($loop);
     $pull = $context->getSocket(ZMQ::SOCKET_PULL);
-    $pull->bind('tcp://127.0.0.1:5555');
+    $pull->bind('tcp://0.0.0.0:5555');
 
     $pull->on('message', array($pusher, 'onReport'));
 
