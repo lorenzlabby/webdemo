bc840f9f62c8013b810a31861fd9917355268bd2
lorenz <lorenzlabby@gmail.com>
2018-09-12 12:44:45 +0300

    realtime

--- static/js/report.js
+++ static/js/report.js
@@ -1,4 +1,4 @@
-var conn = new ab.Session('ws://149.28.138.51:80',
+var conn = new ab.Session('ws://149.28.138.51:8080',
     function(){
         conn.subscribe('arg0', function(topic, data){
             console.log('New article published to category' + topic + ' : ' + data);
