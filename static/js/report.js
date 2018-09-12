9c274bc531c284d1eaa6be7428de17187bd637c6
lorenz <lorenzlabby@gmail.com>
2018-09-12 22:23:16 +0300

    commit

--- static/js/report.js
+++ static/js/report.js
@@ -2,10 +2,7 @@ var conn = new ab.Session('ws://149.28.138.51/ws2/:5555',
     function(){
         conn.subscribe('report', function(topic, data){
             var dataLoop = data.data;
-            console.log(data);
-            for( dataLoop.apps in index, da){
-                console.log(da);
-            };
+            console.log(dataLoop.apps);
 
         });
     },
