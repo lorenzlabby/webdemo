ef8aa3f6833d1b95dfca7d2dbd3f383765718fa1
lorenz <lorenzlabby@gmail.com>
2018-09-12 12:09:59 +0300

    first commit

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL); ?>
-<?php require(dirname(__DIR__) . "/inc/header.php"); ?>
+<?php require("inc/header.php"); ?>
 
         <h1>Hello</h1>
 
-<?php require(dirname(__DIR__) . "/inc/footer.php"); ?>
+<?php require("inc/footer.php"); ?>
